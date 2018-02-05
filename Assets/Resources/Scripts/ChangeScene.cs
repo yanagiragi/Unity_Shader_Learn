74bbd206a14a9425a59fe435f9ce12146f3b43c6
yanagiragi <qaz90473@gmail.com>
2018-02-06 01:27:55 +0800

    Add Menu Scene

---
+++ Assets/Resources/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+    public string sceneName;
+
+    public void click()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+
+        GameObject Canvas = GameObject.Find("MenuCanvas");
+
+        if (Canvas.GetComponentInChildren<UnityEngine.UI.Image>().enabled)
+            Canvas.GetComponentInChildren<UnityEngine.UI.Image>().enabled = false;
+        UnityEngine.UI.Text[] txt = Canvas.GetComponentsInChildren<UnityEngine.UI.Text>();
+        for(int i = 0; i < 2; ++i)
+            if(txt[i].enabled)
+                txt[i].enabled = false;
+    }
+
+}
