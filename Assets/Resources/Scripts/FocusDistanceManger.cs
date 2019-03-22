368163e2398638a4b04bfa370cc98a4952b48c51
yanagiragi <qaz90473@gmail.com>
2019-03-22 13:43:31 +0800

    Update DOF Demo Scene

---
+++ Assets/Resources/Scripts/FocusDistanceManger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusDistanceManger : MonoBehaviour {
+
+    public float distance = 5;
+    public float step = 0.1f;
+
+    public DepthOfField DOF;
+
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+            distance += step;
+        else if (Input.GetKey(KeyCode.S))
+            distance -= step;
+
+        DOF.focalDistance = distance;
+    }
+}
---
+++ Assets/Resources/Scripts/UpdateFocusDistanceUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateFocusDistanceUI : MonoBehaviour {
+
+    UnityEngine.UI.Text text;
+
+    public FocusDistanceManger focusDistanceManger;
+
+    void Start () {
+        text = GetComponent<UnityEngine.UI.Text>();
+    }
+	
+	void LateUpdate () {
+        text.text = "Focus Distance: " + focusDistanceManger.distance;
+    }
+}
