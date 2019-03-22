868d91aadd8c2f59b07a7ccaed17beb0e28487b5
yanagiragi <qaz90473@gmail.com>
2019-03-22 13:52:13 +0800

    Update DOF Demo Scene
    Add Screen Distortion Demos

---
+++ Assets/Resources/Scripts/ScreenDistortionDemo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenDistortionDemo : MonoBehaviour {
+
+    public ScreenDistortion distortion;
+    public float distortionStrength;
+    public float noiseStrength;
+
+    bool isToggle = false;
+
+    void Start () {
+		
+	}
+	
+	public void Toggle () {
+        isToggle = !isToggle;
+        if (isToggle)
+        {
+            distortion._NoiseStrength = noiseStrength;
+            distortion._DistortionStrength = distortionStrength;
+        }
+        else
+        {
+            distortion._NoiseStrength = 0.0f;
+            distortion._DistortionStrength = 0.0f;
+        }
+    }
+    
+}
