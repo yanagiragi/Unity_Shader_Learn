ca6ec86836b16bad1b4558ca0b267e55b6b48849
yanagiragi <qaz90473@gmail.com>
2022-03-19 13:17:16 +0800

    refactor: Create new material in runtime to prevent constantly modify it

--- Assets/Organized/FanboxCard/Scripts/RotateWithMouse.cs
+++ Assets/Organized/FanboxCard/Scripts/RotateWithMouse.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class RotateWithMouse : MonoBehaviour
 {
-    [SerializeField] private Transform Target;
-
     [SerializeField, Range(0, 100)] private float rotateScalar = 10.0f;
 
     [SerializeField] private bool isRevertX = true;
 
     [SerializeField] private bool isRevertY = true;
 
-    [SerializeField] private Material material;
+    [SerializeField] private Material materialRef;
+
+    [SerializeField] private MeshRenderer cardMesh;
+
+    private Material m_material;
+
+    private Material Material
+    {
+        get
+        {
+            if (m_material == null)
+            {
+                m_material = new Material(materialRef);
+                cardMesh.material = m_material;
+            }
+
+            return m_material;
+        }
+    }
 
     void Update()
     {
@@ -30,8 +46,8 @@ public class RotateWithMouse : MonoBehaviour
             position.x *= -1;
         }
 
-        Target.localRotation = Quaternion.Euler(position * rotateScalar);
-        
-        material.SetFloat("_Displacement", position.magnitude);
+        cardMesh.transform.localRotation = Quaternion.Euler(position * rotateScalar);
+
+        Material.SetFloat("_Displacement", position.magnitude);
     }
 }
